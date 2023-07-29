6a0810884da3ceaae218882e1418ead3ed8febe8
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-07-29 00:50:47 -0300

    Add Grounds

--- src/Support/StringExtensions.cs
+++ src/Support/StringExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.RegularExpressions;
 
 namespace Support;
@@ -20,4 +21,54 @@ public static partial class StringExtensions
 
     [GeneratedRegex("([a-z0-9])([A-Z])", RegexOptions.Compiled)]
     private static partial Regex GetPascalCaseRegex();
+
+
+    public static int LevenshteinDistance(this string source, string target)
+    {
+        if (string.IsNullOrEmpty(source))
+        {
+            if (string.IsNullOrEmpty(target))
+            {
+                return 0;
+            }
+
+            return target.Length;
+        }
+
+        if (string.IsNullOrEmpty(target))
+        {
+            return source.Length;
+        }
+
+        if (source.Length > target.Length)
+        {
+            (source, target) = (target, source);
+        }
+
+        var m = target.Length;
+        var n = source.Length;
+        var distance = new int[2, m + 1];
+
+        for (var j = 1; j <= m; j++)
+        {
+            distance[0, j] = j;
+        }
+
+        var currentRow = 0;
+        for (var i = 1; i <= n; ++i)
+        {
+            currentRow = i & 1;
+            distance[currentRow, 0] = i;
+            var previousRow = currentRow ^ 1;
+            for (var j = 1; j <= m; j++)
+            {
+                var cost = (target[j - 1] == source[i - 1] ? 0 : 1);
+                distance[currentRow, j] = Math.Min(
+                    Math.Min(distance[previousRow, j] + 1, distance[currentRow, j - 1] + 1),
+                    distance[previousRow, j - 1] + cost);
+            }
+        }
+
+        return distance[currentRow, m];
+    }
 }
