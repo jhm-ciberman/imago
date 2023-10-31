569e3302b8e07b8a08c1e42f355d9a2a322ff95b
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-10-30 22:40:09 -0300

    Add BuildModeView with house grid gizmo

--- src/Imago/Rendering/ITextureRegion.cs
+++ src/Imago/Rendering/ITextureRegion.cs
@@ -22,3 +22,18 @@ public interface ITextureRegion
     /// </summary>
     public Vector2 BottomRight { get; }
 }
+
+public static class TextureRegionExtensions
+{
+    /// <summary>
+    /// Gets a vector containing the size and offset of the packed texture in texture space coordinates.
+    /// The first two components are the size of the texture
+    /// and the last two components are the offset of the texture in the base texture.
+    /// </summary>
+    /// <returns>A vector containing the size and offset of the packed texture in texture space coordinates.</returns>
+    public static Vector4 GetTextureST(this ITextureRegion region)
+    {
+        Vector2 size = region.BottomRight - region.TopLeft;
+        return new Vector4(size.X, size.Y, region.TopLeft.X, region.TopLeft.Y);
+    }
+}
