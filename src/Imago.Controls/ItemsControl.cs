932d0f209251e20ed872bee558d6e07442e5187b
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2023-11-16 03:13:24 -0300

    Add ItemsSource to ItemsControl

--- src/Imago.Controls/ItemsControl.cs
+++ src/Imago.Controls/ItemsControl.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 using Imago.Rendering.Sprites;
 
 namespace Imago.Controls;
@@ -85,4 +90,119 @@ public abstract class ItemsControl : Control
         base.Update(deltaTime);
     }
 
+    private IItemSource? _itemsSource;
+    private Dictionary<object, Control>? _itemControls;
+
+
+    public IItemSource? ItemsSource
+    {
+        get => this._itemsSource;
+        set
+        {
+            if (this._itemsSource == value) return;
+
+            if (this._itemsSource?.Items is INotifyCollectionChanged oldCollection)
+            {
+                oldCollection.CollectionChanged -= this.ItemsSource_CollectionChanged;
+            }
+
+            this._itemsSource = value;
+
+            if (this._itemsSource?.Items is INotifyCollectionChanged newCollection)
+            {
+                newCollection.CollectionChanged += this.ItemsSource_CollectionChanged;
+            }
+
+            this.InvalidateMeasure();
+            this.OnItemsSourceChanged();
+        }
+    }
+
+    private void ItemsSource_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        switch (e.Action)
+        {
+            case NotifyCollectionChangedAction.Add:
+                this.OnItemsAdded(e.NewItems!);
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                this.OnItemsRemoved(e.OldItems!);
+                break;
+            case NotifyCollectionChangedAction.Replace:
+                this.OnItemsRemoved(e.OldItems!);
+                this.OnItemsAdded(e.NewItems!);
+                break;
+            case NotifyCollectionChangedAction.Move:
+                break;
+            case NotifyCollectionChangedAction.Reset:
+                this.OnItemsReset();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void OnItemsReset()
+    {
+        this.Items.Clear();
+        this._itemControls?.Clear();
+    }
+
+    private void OnItemsAdded(IList items)
+    {
+        this._itemControls ??= new();
+
+        foreach (var item in items)
+        {
+            var control = this.ItemsSource!.CreateItem(item);
+            this.Items.Add(control);
+            this._itemControls.Add(item, control);
+        }
+    }
+
+    private void OnItemsRemoved(IList items)
+    {
+        foreach (var item in items)
+        {
+            var control = this._itemControls![item];
+            this.Items.Remove(control);
+            this._itemControls.Remove(item);
+        }
+    }
+
+    protected virtual void OnItemsSourceChanged()
+    {
+        this.OnItemsReset();
+
+        if (this.ItemsSource is not null)
+        {
+            var list = this.ItemsSource.Items.Cast<object>().ToList();
+            this.OnItemsAdded(list);
+        }
+    }
+}
+
+public class ItemsSource<T> : IItemSource
+{
+    public ItemsSource(IEnumerable<T> items, Func<T, Control> itemTemplate)
+    {
+        this.Items = items;
+        this.ItemTemplate = itemTemplate;
+    }
+
+    public IEnumerable<T> Items { get; }
+    public Func<T, Control> ItemTemplate { get; }
+
+    IEnumerable IItemSource.Items => this.Items;
+
+    Control IItemSource.CreateItem(object item)
+    {
+        return this.ItemTemplate((T)item);
+    }
+}
+
+public interface IItemSource
+{
+    IEnumerable Items { get; }
+    Control CreateItem(object item);
 }
