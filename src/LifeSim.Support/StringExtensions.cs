e1b173b5a20524d680d557ba3b7d19738d07e269
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2024-03-17 13:42:47 -0300

    Move GetDeterministicHashCode to LifeSim.Support

--- src/LifeSim.Support/StringExtensions.cs
+++ src/LifeSim.Support/StringExtensions.cs
@@ -22,6 +22,36 @@ public static partial class StringExtensions
     [GeneratedRegex("([a-z0-9])([A-Z])", RegexOptions.Compiled)]
     private static partial Regex GetPascalCaseRegex();
 
+    /// <summary>
+    /// Gets a deterministic hash code for the string.
+    /// </summary>
+    /// <remarks>
+    /// This method is used to get a deterministic hash code for a string. The default implementation of <see cref="string.GetHashCode()"/>
+    /// is not deterministic and can change between different runs of the program. This method is used to get a deterministic hash code.
+    /// This means the same string will always return the same hash code.
+    /// </remarks>
+    /// <param name="str">The string to get the hash code for.</param>
+    /// <returns>The deterministic hash code for the string.</returns>
+    public static int GetDeterministicHashCode(this string str)
+    {
+        // https://andrewlock.net/why-is-string-gethashcode-different-each-time-i-run-my-program-in-net-core/
+        unchecked
+        {
+            int hash1 = (5381 << 16) + 5381;
+            int hash2 = hash1;
+
+            for (int i = 0; i < str.Length; i += 2)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                if (i == str.Length - 1)
+                    break;
+                hash2 = ((hash2 << 5) + hash2) ^ str[i + 1];
+            }
+
+            return hash1 + (hash2 * 1566083941);
+        }
+    }
+
 
     public static int LevenshteinDistance(this string source, string target)
     {
