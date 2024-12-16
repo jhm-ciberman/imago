1fa83b80e707f451d2060c059c30b4bb1a49787f
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2024-12-16 08:06:11 -0300

    Move SceneChangedEventArgs to its own file for better organization

--- src/LifeSim.Imago/SceneGraph/Stage.cs
+++ src/LifeSim.Imago/SceneGraph/Stage.cs
@@ -6,33 +6,6 @@ using LifeSim.Imago.SceneGraph.Nodes;
 
 namespace LifeSim.Imago.SceneGraph;
 
-/// <summary>
-/// Event arguments for the <see cref="Stage.SceneChanged"/> event.
-/// </summary>
-public class SceneChangedEventArgs : EventArgs
-{
-    /// <summary>
-    /// Gets the old scene.
-    /// </summary>
-    public Scene? OldScene { get; }
-
-    /// <summary>
-    /// Gets the new scene.
-    /// </summary>
-    public Scene? NewScene { get; }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="SceneChangedEventArgs"/> class.
-    /// </summary>
-    /// <param name="oldScene">The old scene.</param>
-    /// <param name="newScene">The new scene.</param>
-    public SceneChangedEventArgs(Scene oldScene, Scene newScene)
-    {
-        this.OldScene = oldScene;
-        this.NewScene = newScene;
-    }
-}
-
 public class Stage
 {
     /// <summary>
