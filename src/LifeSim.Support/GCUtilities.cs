c4831a5eb71ef189a5c4fec9473684f26f27e790
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2025-03-30 17:06:09 -0300

    Simplify world generator

---
+++ src/LifeSim.Support/GCUtilities.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime;
+
+namespace LifeSim.Support;
+
+public static class GCUtilities
+{
+    /// <summary>
+    /// Executes a full garbage collection and compacts the large object heap.
+    /// This is useful for reducing memory fragmentation and reclaiming memory.
+    /// </summary>
+    public static void AgressiveGC()
+    {
+        GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+        GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, blocking: true, compacting: true);
+    }
+}
