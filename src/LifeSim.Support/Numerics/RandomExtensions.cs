310f498a971c73ba798fc8995382c7601fea07e6
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2025-05-11 01:15:00 -0300

    wip

--- src/LifeSim.Support/Numerics/RandomExtensions.cs
+++ src/LifeSim.Support/Numerics/RandomExtensions.cs
@@ -2,6 +2,7 @@ namespace LifeSim.Support.Numerics;
 
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 
 public static class RandomExtensions
 {
@@ -31,5 +32,35 @@ public static class RandomExtensions
     {
         if (min.X >= max.X || min.Y >= max.Y) throw new ArgumentException("Min must be less than max.");
         return new Vector2Int(random.Next(min.X, max.X), random.Next(min.Y, max.Y));
+    }
+
+    /// <summary>
+    /// Returns a random <see cref="Vector2"/> from the specified range.
+    /// </summary>
+    /// <param name="random">The random number generator.</param>
+    /// <param name="min">The minimum value (inclusive).</param>
+    /// <param name="max">The maximum value (exclusive).</param>
+    /// <exception cref="ArgumentException">Thrown when min is greater than or equal to max.</exception>
+    /// <returns>A random <see cref="Vector2"/> within the specified range.</returns>
+    public static Vector2 NextVector2(this Random random, Vector2 min, Vector2 max)
+    {
+        if (min.X >= max.X || min.Y >= max.Y) throw new ArgumentException("Min must be less than max.");
+        return new Vector2(
+            (float)(random.NextDouble() * (max.X - min.X) + min.X),
+            (float)(random.NextDouble() * (max.Y - min.Y) + min.Y));
+    }
+
+    /// <summary>
+    /// Returns a random <see cref="float"/> from the specified range.
+    /// </summary>
+    /// <param name="random">The random number generator.</param>
+    /// <param name="min">The minimum value (inclusive).</param>
+    /// <param name="max">The maximum value (exclusive).</param>
+    /// <exception cref="ArgumentException">Thrown when min is greater than or equal to max.</exception>
+    /// <returns>A random <see cref="float"/> within the specified range.</returns>
+    public static float NextSingle(this Random random, float min, float max)
+    {
+        if (min >= max) throw new ArgumentException("Min must be less than max.");
+        return (float)(random.NextDouble() * (max - min) + min);
     }
 }
